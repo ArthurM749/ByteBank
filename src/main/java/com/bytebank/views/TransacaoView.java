59fe4b854d6bdce72eb9ab74567165357a9ab781
ArthurM749 <arthurmccomp@gmail.com>
2026-08-18 22:20:38 -0300

    Refactor TransacaoView layout and improve transaction handling

--- src/main/java/com/bytebank/views/TransacaoView.java
+++ src/main/java/com/bytebank/views/TransacaoView.java
@@ -1,9 +1,12 @@
 package com.bytebank.views;
 
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.time.LocalDateTime;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -17,23 +20,52 @@ import com.bytebank.models.Transacao;
 
 public class TransacaoView extends JFrame {
   private TransacaoController controller = new TransacaoController();
-  private int id = 0;
+  private String[] tipos = {"PIX", "Dinheiro"};
 
   public TransacaoView() {
     setTitle("Sistema de Transações ByteBank");
     setSize(600, 400);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-    JPanel panel = new JPanel();
-    JLabel label = new JLabel("Valor:");
-    JTextField textField = new JTextField(10);
-    JButton button = new JButton("Salvar");
+    //Paineis
 
-    panel.add(label);
-    panel.add(textField);
-    panel.add(button);
+    JPanel panelTopo = new JPanel(new BorderLayout());
+    panelTopo.setBorder(BorderFactory.createEmptyBorder(15,15,15,15));
+    JPanel panelFormulario = new JPanel(new GridLayout(5, 2,5,5));
+    JPanel panelBotoes = new JPanel();
 
-    add(panel, BorderLayout.NORTH);
+    //CAMPOS
+    
+    JTextField campoId = new JTextField();
+    JTextField campoValor = new JTextField();
+    JComboBox<String> comboTipo = new JComboBox<>(tipos);
+
+    //MONTANDO FORMULARIO
+
+    panelFormulario.add(new JLabel("ID (Para buscar/deletar): "));
+    panelFormulario.add(campoId);
+    panelFormulario.add(new JLabel("Valor (R$): "));
+    panelFormulario.add(campoValor);
+    panelFormulario.add(new JLabel("Tipo: "));
+    panelFormulario.add(comboTipo);
+
+    //Botões
+
+    JButton botaoSalvar = new JButton("Salvar");
+    JButton botaoBuscar = new JButton("Buscar ID");
+    JButton botaoEditar = new JButton("Editar");
+    JButton botaoDeletar = new JButton("Deletar");
+    
+    panelBotoes.add(botaoSalvar);
+    panelBotoes.add(botaoBuscar);
+    panelBotoes.add(botaoEditar);
+    panelBotoes.add(botaoDeletar);
+    
+    panelTopo.add(panelFormulario, BorderLayout.CENTER);
+    panelTopo.add(panelBotoes, BorderLayout.SOUTH);
+
+
+    add(panelTopo, BorderLayout.NORTH);
 
     String[] colunas = {"ID", "Valor", "Tipo", "Data"};
 
@@ -45,13 +77,16 @@ public class TransacaoView extends JFrame {
     add(scrollPane, BorderLayout.CENTER);
 
     // Açao para adicionar na table
-    button.addActionListener((args) -> {
-      double valor = Double.parseDouble(textField.getText());
-      Transacao novaTransacao = new Transacao(id++, valor, "PIX", LocalDateTime.now().toString());
+    botaoSalvar.addActionListener((args) -> {
+      int novoId = controller.listarTodos().size() + 1;
+      
+      double valor = Double.parseDouble(campoValor.getText());
+      Transacao novaTransacao = new Transacao(novoId, valor, "PIX", LocalDateTime.now().toString());
 
       controller.adicionarTransacao(novaTransacao);
-      textField.setText("");
+      campoValor.setText("");
     });
+      
   }
   
 }
